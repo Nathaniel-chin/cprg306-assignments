169ec2dcec0a174a7d535d9a3647aa289ff58300
Nathaniel Chin <Nathaniel.Chin@edu.sait.ca>
2023-10-04 12:22:36 -0600

    first commit

---
+++ app/StudentInfo.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Link from 'next/link';
+
+function StudentInfo() {
+  
+  return (
+    <div>
+      <h1>Student Information</h1>
+      <p>
+        Name: Nathaniel CHin
+      </p>
+      <p>
+        Course Section: cprg 306 E
+      </p>
+      <p>
+        GitHub Repository: adsjfladsfjalk;fdsjf
+      </p>
+    </div>
+  );
+}
+
+export default StudentInfo;
--- app/week2/page.js
+++ app/week2/page.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import Link from 'next/link';
-import StudentInfo from './StudentInfo';
+import StudentInfo from '../StudentInfo';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <p>My Shopping list</p>
+      
       <div>
         <h1>CPRG 306: Web Development 2 - Assignments</h1>
         < StudentInfo/>
-        <Link href="/week2">Go to Week 2</Link>
       </div>
     </main>
   );
